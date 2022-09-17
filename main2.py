bb6e5826fbcd7645edd031561e634f36aa486f27
Fernando  Pérez <113327544+FernandPe@users.noreply.github.com>
2022-09-17 23:51:27 +0000

    Cambio de titulo

--- main2.py
+++
@@ -1,9 +0,0 @@
-numero=int(input ("Digíte un número :"))
-if numero > 0 :
-  print ("El número es positivo")
-
-elif numero ==0:
-    print("El número es 0 neutro")
-
-else: 
-  print("El número es negativo")
